1329173ec79c29c1cc24e0fe47c72c841e625e91
Ian Mobbs <ianmobbs@gmail.com>
2017-09-24 15:18:44 -0500

    Simplified adding towers and enemies to game

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,8 @@
-var game = new Phaser.Game(1000, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(1000, 600, Phaser.AUTO, ''),
+    lives = 100,
+    coins = 1000;
 
-let grid = {}
+
 var gameState = {
     preload: function() {
         // Map and sidebar for game functionality
@@ -8,34 +10,54 @@ var gameState = {
         game.load.image('sidebar', '../assets/sidebar.png')
         
         //Tower sprites
-        game.load.image('nest', '../assets/nest.png')
+        game.load.json('towers', 'js/towers.json');
 
         // Enemy sprites
-        game.load.image('troll', '../assets/troll.png')
+        game.load.json('enemies', 'js/enemies.json');
     },
     create: function() {
         // Add game interface
         game.add.sprite(0, 0, 'map')
         game.add.sprite(800, 0, 'sidebar')
-
-        // Load in towers and enable drag and drop interactivity
-        var towers = [
-            game.add.sprite(800, 40, 'nest')
-        ];
-        for (var tIndex = 0; tIndex < towers.length; tIndex++) {
-            // https://phaser.io/examples/v2/input/snap-on-drag
-            var tower = towers[tIndex];
-            tower.inputEnabled = true;
-            tower.input.enableDrag();
-            tower.input.enableSnap(32, 32, true, true);
+        
+        // Tower sprites
+        var towers = game.cache.getJSON('towers');
+        console.log(towers);
+        for (tIndex in towers) {
+            // Load the tower and the game image
+            let tower = towers[tIndex];
+            game.load.image(tower.name, tower.sprite)
+            
+            // Create the sprite for the towers and enable drag and drop
+            var towerSprite = game.add.sprite(800, 40 + tIndex * 75, tower.name);
+            towerSprite.inputEnabled = true;
+            towerSprite.input.enableDrag();
+            towerSprite.input.enableSnap(32, 32, true, true);
         }
-        // Add in one monster
+
+        // Enemy sprites
+        var enemies = game.cache.getJSON('enemies');
+        for (eIndex in enemies) {
+            // Include enemy and sprite in animation
+            let enemy = enemies[eIndex];
+            game.load.image(enemy.name, enemy.sprite)
+            game.add.sprite(enemy.name, enemy.sprite)
+        }
+
+        // Add game information
+        game.add.text(805, 510,
+            'Coins: ' + coins.toString() + '\n' +
+            'Lives: ' + lives.toString(),
+            {
+                font: '15px Arial',
+            }
+        )
+        
+        // Add in one monster to test enemy sprite creation
         var t1 = game.add.sprite(0, 0, 'troll');
 
     },
-    update: function() {
-
-    }
+    update: function() {}
 }
 
 game.state.add('gameState', gameState)
