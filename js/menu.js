7347b44f5ba04f3ebf3ed0d15cbd33e441a8e1ac
Xindi Xu <xindixu@utexas.edu>
2017-11-05 10:06:15 -0600

    menu change

--- js/menu.js
+++ js/menu.js
@@ -5,13 +5,13 @@ var menuState = {
 
     create: function(){
         
-        var name = game.add.text(80,80,"Defend the Throne",{font: '100px Arial', fill: '#FFFFFF'});
+        var name = game.add.text(40,80,"Defend the Throne",{font: '80px Arial', fill: '#FFFFFF'});
         
         var button = game.add.button(game.world.centerX - 100, 400, 'start', this.start, this, 0,1,0);
-        var label = game.add.text(game.world.centerX - 100,450,"Play",{font: '40px Arial', fill: '#FFFFFF'});
+        var label = game.add.text(game.world.centerX - 100,450,"Play",{font: '20px Arial', fill: '#FFFFFF'});
         
         var button1 = game.add.button(game.world.centerX +50 , 400, 'start', this.tutorial, this, 0,1,0);
-        var label1 = game.add.text(game.world.centerX + 50,450,"Tutorial",{font: '40px Arial', fill: '#FFFFFF'});
+        var label1 = game.add.text(game.world.centerX + 50,450,"Tutorial",{font: '20px Arial', fill: '#FFFFFF'});
         
     },
     
