c19dce807c4ba6eec8dca611933539b971a232aa
Xindi Xu <xindixu@utexas.edu>
2017-11-30 12:42:22 -0600

    load in more buttons

--- js/lose.js
+++ js/lose.js
@@ -2,7 +2,7 @@ var loseState = {
     create: function(){
         var info = game.add.text(80,80,"You lose!",{font: "100px Arial", fill: '#FFFFFF'});
         
-        var button = game.add.button(game.world.centerX - 30, 400, 'start', this.start, this, 0,1,0);
+        var button = game.add.button(game.world.centerX - 30, 400, 'restart', this.start, this, 0,1,0);
     },
     
     start: function(){
